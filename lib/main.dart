cb98f995828f2c0e42991af6ce7da55a87991f4b
Madi <havkpzz@gmail.com>
2024-09-04 14:31:55 +0500

    feature(main.dart): add image card

--- lib/main.dart
+++ lib/main.dart
@@ -14,59 +14,77 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color defaultColor = const Color.fromRGBO(60, 117, 62, 1);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromRGBO(60, 117, 62, 1),
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: defaultColor),
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(60, 117, 62, 1),
+          backgroundColor: defaultColor,
         ),
         body: Container(
-            padding: const EdgeInsets.fromLTRB(15, 40, 15, 40),
-            color: const Color.fromRGBO(60, 117, 62, 1),
+            color: defaultColor,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Image.asset(
-                      "assets/images/sandwich.png",
-                      width: 220,
-                      height: 220,
-                    ),
-                    const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 16, 0, 6),
-                        child: Text(
-                          "Quick,\nFresh & \nDelicious",
-                          softWrap: true,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold,
-                              height: 1.4),
+                    Card(
+                        margin: EdgeInsets.zero,
+                        color: const Color.fromRGBO(96, 150, 100, 1),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(42),
+                                bottomRight: Radius.circular(42))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 48, horizontal: 52),
+                          child: Image.asset(
+                            "assets/images/sandwich.png",
+                            width: 220,
+                            height: 220,
+                          ),
                         )),
-                    const Text(
-                      "Made by hand, from scratch, with love. Green food.",
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
+                    const Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 16, horizontal: 15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Quick, Fresh & Delicious",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  height: 1.4),
+                            ),
+                            Text(
+                              "Made by hand, from scratch, with love.Green food.",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 14),
+                            ),
+                          ],
+                        )),
                   ],
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const Column(
-                      children: [],
-                    ),
-                    StyledFilledButton(
-                        title: "Get started",
-                        onPressed: handleRedirectToToLogin)
-                  ],
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(15, 0, 15, 40),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      const Column(
+                        children: [],
+                      ),
+                      StyledFilledButton(
+                          title: "Get started",
+                          onPressed: handleRedirectToToLogin)
+                    ],
+                  ),
                 )
               ],
             )),
